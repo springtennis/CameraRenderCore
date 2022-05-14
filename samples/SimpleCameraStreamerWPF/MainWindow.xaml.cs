e277991c78b7b0e9f26b91a89d3b383f89f1513e
YongJae_Kim <yjkim@springtennis.co>
2022-05-15 00:46:04 +0900

    add SimpleCameraStreamerWPF samples (not stable)

---
+++ samples/SimpleCameraStreamerWPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace SimpleCameraStreamerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void On_UIReady(object sender, RoutedEventArgs e)
+        {
+            PresentationSource source = PresentationSource.FromVisual(this);
+
+            SentechCameraCoreWPF.ControlHost cameraHost = new SentechCameraCoreWPF.ControlHost(
+                ControlHostElement.ActualHeight,
+                ControlHostElement.ActualWidth);
+
+            ControlHostElement.Child = cameraHost;
+        }
+    }
+}
